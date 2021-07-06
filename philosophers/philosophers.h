533ca2e30fce8e5ad910d78216d419bee40228d4
0masiza0 <rlar7007@gmail.com>
2021-07-06 17:18:33 +0300

    Solution of Philosophers

--- philosophers/philosophers.h
+++ philosophers/philosophers.h
@@ -31,24 +31,41 @@ class Fork {
 
 class Philosopher {
  public:
-  Philosopher(size_t /*id*/, Fork* /*left_fork*/, Fork* /*right_fork*/) {
-    // Your code
-  }
+  Philosopher(size_t id, Fork* left_fork, Fork* right_fork)
+    : id_(id)
+    , left_fork_(left_fork)
+    , right_fork_(right_fork)
+    {
+      if (left_fork_->Id() > right_fork_->Id()) {
+          std::swap(left_fork_, right_fork_);
+      }
+    }
 
   size_t Id() const {
-    // Your code
-    return -1;
+    return id_;
   }
 
   void Eat() {
-    // Your code
+    while (!left_locked_) {
+        left_locked_ = left_fork_->TryGet();
+    }
+    while (!right_locked_) {
+        right_locked_ = right_fork_->TryGet();
+    }
   }
 
   void Think() {
-    // Your code
+    right_fork_->Put();
+    right_locked_ = false;
+    left_fork_->Put();
+    left_locked_ = false;
   }
 
  private:
-  // Your code
+  const size_t id_;
+  Fork* left_fork_;
+  Fork* right_fork_;
+  bool left_locked_ = false;
+  bool right_locked_ = false;
 };
 
