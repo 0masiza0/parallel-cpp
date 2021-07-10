06459ca0c3e038f6d99fa1103a52dad3c5beb695
0masiza0 <rlar7007@gmail.com>
2021-07-10 13:15:17 +0300

    Solution of DeadLock

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <atomic>
+#include <chrono>
+#include <mutex>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 class Deadlock {
  public:
@@ -7,14 +13,23 @@ class Deadlock {
   }
 
   void ThreadOne() {
-    // Your
+      mutex1_.lock();
+      std::this_thread::sleep_for(10ms);
+      mutex2_.lock();
+      mutex1_.unlock();
+      mutex2_.unlock();
   }
 
   void ThreadTwo() {
-    // Your
+      mutex2_.lock();
+      std::this_thread::sleep_for(10ms);
+      mutex1_.lock();
+      mutex2_.unlock();
+      mutex1_.unlock();
   }
 
  private:
-  // Your
+    std::mutex mutex1_;
+    std::mutex mutex2_;
 };
 
